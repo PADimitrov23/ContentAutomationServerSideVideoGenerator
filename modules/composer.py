ba690b53059e70401a8b222c368d1a4ef614f96d
Peter Dimitrov <PADimitrov23@codingburgas.bg>
2026-08-19 03:26:57 +0300

    fix xfade timebase mismatch

--- modules/composer.py
+++ modules/composer.py
@@ -172,24 +172,27 @@ class Composer:
             else:
                 trans_dur = 0.0
 
+            next_v = next_clip.video.filter("settb", "AVTB")
+            next_a = next_clip.audio.filter("settb", "AVTB")
+
             if trans_dur > 0:
                 offset = max(0, current_dur - trans_dur)
                 v_stream = ffmpeg.filter(
-                    [v_stream, next_clip.video],
+                    [v_stream, next_v],
                     "xfade",
                     transition="fade",
                     duration=trans_dur,
                     offset=offset,
                 )
                 a_stream = ffmpeg.filter(
-                    [a_stream, next_clip.audio],
+                    [a_stream, next_a],
                     "acrossfade",
                     d=trans_dur,
                 )
                 current_dur = (current_dur + next_dur) - trans_dur
             else:
-                v_stream = ffmpeg.concat(v_stream, next_clip.video, v=1, a=0)
-                a_stream = ffmpeg.concat(a_stream, next_clip.audio, v=0, a=1)
+                v_stream = ffmpeg.concat(v_stream, next_v, v=1, a=0)
+                a_stream = ffmpeg.concat(a_stream, next_a, v=0, a=1)
                 current_dur = current_dur + next_dur
 
         try:
